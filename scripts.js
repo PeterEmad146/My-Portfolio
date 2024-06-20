39e1f5404d5a155863986a3aa0d2f18623e62bf5
Peter Emad <emadp626@gmail.com>
2024-06-20 09:47:33 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,38 @@
+document.querySelectorAll('.nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1); 
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
+
+
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+
+    // Check for partial visibility: Any part of the element is visible
+    return (
+        rect.top <= window.innerHeight &&
+        rect.bottom >= 0
+    );
+}
+
+// Function to handle scroll event
+function handleScroll() {
+    var elements = document.querySelectorAll('.div-to-scroll');
+    elements.forEach(function(el) {
+        if (isElementInViewport(el)) {
+            el.classList.add('visible');
+        } else {
+            el.classList.remove('visible');
+        }
+    });
+}
+
+// Initial check on page load
+document.addEventListener('DOMContentLoaded', handleScroll);
+// Check on scroll
+document.addEventListener('scroll', handleScroll);
